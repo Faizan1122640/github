2d517223e8729de92bcab0ac6abd5b44781c9a08
FAIZAN <saleemsonfaizan@gmail.com>
2025-07-01 14:43:45 +0500

    ok

--- Main.js
+++ Main.js
@@ -1,1 +1,21 @@
-hi
+//  a function that finds the largest number in an array.
+let array = [1, 2, 3, 4, 5, 60, 7, 8, 9, 10];
+function Findlargestnumber(arr) {
+    let largest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+// Write a function that adds two numbers and returns the result.
+   let n1 = prompt("Enter first number:");
+   let n2 = prompt("Enter second number:");
+   function addTwoNumbers(n1, n2) {
+       return parseFloat(n1) + parseFloat(n2);
+   }
+   let sum = addTwoNumbers(n1, n2);
+   return sum;
+   
